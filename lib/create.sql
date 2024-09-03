1456b32a5d51560d8a15416b26cbab291bd3a30d
Marlon Munoz <marlon@marlonshade.com>
2024-09-02 22:55:18 -0400

    sqlite lab completed

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+id INTERGER,
+name TEXT,
+age INTEGER,
+sex TEXT,
+color TEXT,
+temperament TEXT,
+alive BOOLEAN
+);
---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (1, 'Mr. Chocolate', 20, 'M', 'dark brown', 'grumpy', True);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (2, 'Rowdy', 10, 'M', 'black', 'rowdy', True);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (3, 'Tabitha', 30, 'F', 'brown', 'nice', True);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (4, 'Sargeant Brown', 15, 'M', 'green', 'sleepy', True);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (5, 'Melissa', 20, 'F', 'blue', 'nice', True);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (6, 'Grinch', 100, 'M', 'green', 'grumpy', True);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (7, 'Wendy', 40, 'F', 'blue', 'nice', True);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (8, NULL, NULL, NULL, NULL, NULL, NULL);
